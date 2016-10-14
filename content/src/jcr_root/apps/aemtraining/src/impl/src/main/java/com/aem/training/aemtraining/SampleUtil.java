e677aca5bd8e441f8037b2d2581613eb3fdc0c5f
Tagore Sai Vytla <Tagore Sai Vytla>
2016-10-14 15:06:45 -0400

    changes

---
+++ content/src/jcr_root/apps/aemtraining/src/impl/src/main/java/com/aem/training/aemtraining/SampleUtil.java
@@ -0,0 +1,11 @@
+package com.aem.training.aemtraining;
+
+/**
+ * Utility class used by script.
+ */
+public class SampleUtil {
+
+    public static String getText() {
+        return "Hello World.";
+    }
+}
